2e547b866033421a8d2c0120ec4441598ab5c0a0
pedrobarretto <pedro@barretto.com.br>
2022-03-06 13:35:11 -0300

    feat: Add cors * on app

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 
 import { connect } from './mongo';
 import { router } from './routes';
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
+app.options('*', cors());
+
 app.use(bodyParser.json());
 app.use(router);
 
